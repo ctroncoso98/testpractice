be4d377e1a58687c11cb96899240334e4b43a716
Christal Troncoso <ciriat0716@gmail.com>
2023-06-26 11:58:18 -0700

    Initial Commit

---
+++ testpractice/testpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testpracticeApp.swift
+//  testpractice
+//
+//  Created by Christal Troncoso on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct testpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
